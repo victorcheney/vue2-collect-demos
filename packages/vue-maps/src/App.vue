7437d78bd9222257b5e464fdc83bb38715dd4aee
chenfengtao <cft12534@cnki.net>
2022-08-30 15:55:33 +0800

    feat: 引入tailwindcss

--- packages/vue-maps/src/App.vue
+++ packages/vue-maps/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      |
       <router-link to="/echartmap">EchartMap</router-link>
     </div>
     <router-view />
