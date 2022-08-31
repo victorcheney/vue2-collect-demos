b15d8fd643a4f2f12365962e449024aa6e807c10
chenfengtao <cft12534@cnki.net>
2022-08-31 15:53:43 +0800

    feat: koa-websocket项目初始化

---
+++ packages/koa-websocket/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 0,
+  },
+}
