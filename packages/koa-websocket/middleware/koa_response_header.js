9db619184e2688a0bb373a4bd2fb891129fc0d57
chenfengtao <cft12534@cnki.net>
2022-09-02 17:11:59 +0800

    feat: 配置跨域

--- packages/koa-websocket/middleware/koa_response_header.js
+++ packages/koa-websocket/middleware/koa_response_header.js
@@ -3,11 +3,13 @@
  * @Author: chenfengtao
  * @Date: 2022-08-31 16:02:10
  * @LastEditors: chenfengtao
- * @LastEditTime: 2022-08-31 16:31:56
+ * @LastEditTime: 2022-09-02 15:17:47
  */
 module.exports = async (ctx, next) => {
   const contentType = 'application/json; charset=utf-8'
   ctx.set('Content-Type', contentType)
-  ctx.response.body = '{"success": true}'
+  ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE')
+  // ctx.response.body = '{"success": true}'
   await next()
 }
